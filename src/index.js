66ca43e1c94525114d13a5b6a84606e044ee9f3b
unknown <yablonjkas@gmail.com>
2022-10-19 01:13:55 +0300

    feat:morse-decoder

--- src/index.js
+++ src/index.js
@@ -1,69 +1,57 @@
-
+const MORSE_TABLE = {
+    '.-':     'a',
+    '-...':   'b',
+    '-.-.':   'c',
+    '-..':    'd',
+    '.':      'e',
+    '..-.':   'f',
+    '--.':    'g',
+    '....':   'h',
+    '..':     'i',
+    '.---':   'j',
+    '-.-':    'k',
+    '.-..':   'l',
+    '--':     'm',
+    '-.':     'n',
+    '---':    'o',
+    '.--.':   'p',
+    '--.-':   'q',
+    '.-.':    'r',
+    '...':    's',
+    '-':      't',
+    '..-':    'u',
+    '...-':   'v',
+    '.--':    'w',
+    '-..-':   'x',
+    '-.--':   'y',
+    '--..':   'z',
+    '.----':  '1',
+    '..---':  '2',
+    '...--':  '3',
+    '....-':  '4',
+    '.....':  '5',
+    '-....':  '6',
+    '--...':  '7',
+    '---..':  '8',
+    '----.':  '9',
+    '-----':  '0',
+    ' '    : ' '
+};
 
 function decode(expr) {
    
-    const MORSE_TABLE = {
-        '.-':     'a',
-        '-...':   'b',
-        '-.-.':   'c',
-        '-..':    'd',
-        '.':      'e',
-        '..-.':   'f',
-        '--.':    'g',
-        '....':   'h',
-        '..':     'i',
-        '.---':   'j',
-        '-.-':    'k',
-        '.-..':   'l',
-        '--':     'm',
-        '-.':     'n',
-        '---':    'o',
-        '.--.':   'p',
-        '--.-':   'q',
-        '.-.':    'r',
-        '...':    's',
-        '-':      't',
-        '..-':    'u',
-        '...-':   'v',
-        '.--':    'w',
-        '-..-':   'x',
-        '-.--':   'y',
-        '--..':   'z',
-        '.----':  '1',
-        '..---':  '2',
-        '...--':  '3',
-        '....-':  '4',
-        '.....':  '5',
-        '-....':  '6',
-        '--...':  '7',
-        '---..':  '8',
-        '----.':  '9',
-        '-----':  '0',
-    };
-    
-    function decode(expr) {
-       
-        let String = expr.match(/.{1,10}/g);
-        String = String.toString().replace(/\b0+/g, "");
-        currstr = "";
-        let array = [];
-        for (var i = 0; i < String.length; ++i) {
-           currstr += expr[i];
-     
-           if (currstr.length == 2) {
-              array.push(currstr);
-              
-              currstr = "";
-           }
-        }
-        array = String.replace(/\b0+/g, "").replace(/10/g,".").replace(/11/g,'-').replace(/,/g,' ');
-        var rep = array.replace(/([.-]+[-.]*)/g, (_, x) => MORSE_TABLE [x]).replace();
-        result = rep.replace(/ /g, '').replace(/\*/g, ' ').replace(/          /g, ' '); // write your solution here
-        }
-    
-    
-    module.exports = {
-        decode
-    }}
-    
-   
+
+    let letter = '';
+    for (let i=0; i<expr.length; i=i+10) {
+        
+        letter = letter + (MORSE_TABLE[expr.slice(i, i+10).replace(/00/g, '').replace(/10/g, '.').replace(/11/g, '-').replace(/\*{10}/g, ' ')]);
+    }
+   return letter;
+      
+}
+
+
+
+module.exports = {
+    decode
+}
